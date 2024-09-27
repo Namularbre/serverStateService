122448a9cf302ba3c71f91fe047cf3ca67d88173
namu <namulabre@gmail.com>
2024-09-27 23:05:24 +0200

    Added basic web app setup

--- serverStateService.go
+++ serverStateService.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"os"
+	"serverStateService/handlers"
+)
 
+func main() {
+	http.HandleFunc("/", handlers.GetServersHandler)
+
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		log.Fatalln("Error listening")
+	}
 }
